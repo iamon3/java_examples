8931fb779a24e3b92bf5a85d830f0700b4dd5117
free.coder <iamon3.free@gmail.com>
2026-01-16 07:22:43 +0530

    Demo 1) Stream.of() 2) IntStream

--- java21/src/main/java/com/freeorg/java21/streams/CircleWithMaxUniquelyTaggedPoints.java
+++ java21/src/main/java/com/freeorg/java21/streams/CircleWithMaxUniquelyTaggedPoints.java
@@ -1,22 +1,22 @@
 package com.freeorg.java21.streams;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class CircleWithMaxUniquelyTaggedPoints {
 
     public int solution(String S, int[] X, int[] Y) {
-        char [] tags = S.toCharArray();
+        char[] tags = S.toCharArray();
 
-        record Point(int x, int y, int distance, char tag){};
+        record Point(int x, int y, int distance, char tag) {
+        }
+        ;
         List<Point> points = new ArrayList<>();
 
-        for (int i=0 ; i < X.length ; i++){
-            points.add(new Point(X[i], Y[i], calcualteDistance(X[i], Y[i]),tags[i]));
+        for (int i = 0; i < X.length; i++) {
+            points.add(new Point(X[i], Y[i], calcualteDistance(X[i], Y[i]), tags[i]));
         }
 
         List<Point> sortedPoints = points.stream()
@@ -32,35 +32,39 @@ public class CircleWithMaxUniquelyTaggedPoints {
         }
 
         return visitedTags.size();
+    }
+
+    int calcualteDistance(int x, int y) {
+        return x * x + y * y;
     }
 
     public int solution2(String S, int[] X, int[] Y) {
-        char [] tags = S.toCharArray();
+        char[] tags = S.toCharArray();
 
-        record Point(int x, int y, int distance, char tag){};
-        List<Point> points = new ArrayList<>();
+        System.out.println("Following stream simplt prints references to arrays of 3 streams, as Stream elements are arrays not values.");
+        Stream.of(Arrays.stream(X).boxed(),
+                        Arrays.stream(Y).boxed(),
+                        S.chars().mapToObj(c -> (char) c)
+                )
+                .forEach(System.out::println);
 
-        for (int i=0 ; i < X.length ; i++){
-            points.add(new Point(X[i], Y[i], calcualteDistance(X[i], Y[i]),tags[i]));
-        }
-
-        List<Point> sortedPoints = points.stream()
+        System.out.println("Following stream is what we want as IntStream are faster than Stream.");
+        record Point(int x, int y, int distance, char tag) { };
+        Set<Character> seenTags = new HashSet<>();
+        IntStream.range(0,
+                        Math.min(Math.min(X.length, Y.length), tags.length))
+                .mapToObj(i -> new Point(X[i], Y[i], calcualteDistance(X[i], Y[i]), tags[i]))
                 .sorted(Comparator.comparing(Point::distance))
-                .collect(Collectors.toList());
+                .takeWhile(p -> seenTags.add(p.tag()))
+                .forEach(System.out::println);
 
-        Set<Character> visitedTags = new HashSet<>();
-        for (Point point : sortedPoints) {
-            if (visitedTags.contains(point.tag())) {
-                break;
-            }
-            visitedTags.add(point.tag());
-        }
-
-        return visitedTags.size();
+        return -1;
     }
 
-    int calcualteDistance(int x, int y){
-        return x*x + y*y;
+    public static void main(String[] args) {
+        CircleWithMaxUniquelyTaggedPoints s = new CircleWithMaxUniquelyTaggedPoints();
+        s.solution("ABCDAE", new int[]{2,4,3,4,5,3}, new int[]{4,2,1,3,1,4});
+        s.solution2("ABCDAE", new int[]{2,4,3,4,5,3}, new int[]{4,2,1,3,1,4});
     }
 
 }
