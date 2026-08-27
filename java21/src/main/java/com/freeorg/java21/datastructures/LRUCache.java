06ba732d6025e2e92b6aa07b8da6bfab0d2ec693
free.coder <iamon3.free@gmail.com>
2026-08-28 00:29:43 +0530

    Practice String Arrays String Builder

--- java21/src/main/java/com/freeorg/java21/datastructures/LRUCache.java
+++ java21/src/main/java/com/freeorg/java21/datastructures/LRUCache.java
@@ -1,6 +1,10 @@
 package com.freeorg.java21.datastructures;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class LRUCache<K, V> {
